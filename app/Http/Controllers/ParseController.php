3623421e75f03a90b9c16998faa55719c67451f3
Eugen <evgeniy.sayenko@gmail.com>
2018-03-23 16:39:37 +0200

    Готовы парсеры, индекс+пагинация, бот уже стучится в Телеграм, но ещё не оформлен

--- app/Http/Controllers/ParseController.php
+++ app/Http/Controllers/ParseController.php
@@ -14,6 +14,7 @@ use Carbon\Carbon;
 class ParseController extends Controller
 {
     public function index()
+        
     {
         $links =  DB::table('links')->orderBy('id', 'desc')->paginate(30);
 //        $links = $links->sortByDesc('id');
